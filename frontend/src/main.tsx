028ca871eca3be4348df1bc7303b01352076af5a
serotonin1987 <samilnafisa@gmail.com>
2026-04-23 18:24:17 +0500

    feat: добавлена базовая разметка трекера

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+
+const root = document.getElementById("root");
+
+if (!root) {
+  throw new Error("Элемент с id 'root' не найден");
+}
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
